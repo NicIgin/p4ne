9b680f1717daff37097af7f4e876f2d9fec67fd8
Heldenberg <tot1991@yandex.ru>
2025-03-21 16:52:31 +0300

    1

---
+++ Lab1.5/logs.py
@@ -0,0 +1,17 @@
+import glob
+
+listIP = []
+
+for i in glob.glob("*.log"):
+    f = open(i)
+    lines = list(f)
+    for j in lines:
+        posSubstr = j.find("ip address ")
+        if posSubstr > -1 :
+            ip = j.strip()[posSubstr + 10:].split(" ")[0]
+            b = j.strip()[posSubstr + 10:].split(" ")
+            if len(b) > 1 :
+                mask = j.strip()[posSubstr + 10:].split(" ")[1]
+            else:
+                mask = ""
+            print(ip + " " + mask)
